34f9b7741b60a433c94dbeda227b0e32f6128f0f
ekaterinakur <ekaterina1.kur@gmail.com>
2025-11-27 18:47:59 +0200

    [Task 1]: changed the order of right panels in the longitudinal mode

--- modes/longitudinal/src/index.ts
+++ modes/longitudinal/src/index.ts
@@ -29,7 +29,7 @@ export const longitudinalInstance = {
   props: {
     ...basicLayout.props,
     leftPanels: [tracked.thumbnailList],
-    rightPanels: [cornerstone.segmentation, tracked.measurements],
+    rightPanels: [tracked.measurements, cornerstone.segmentation],
     viewports: [
       {
         namespace: tracked.viewport,
